5daa06c750460e0f2a449e44d07fd8821076b786
Emerson <emersonlucenaoficial@gmail.com>
2022-05-01 21:54:08 -0300

    removed useless contents

--- API/src/assets/ubeAPI.ts
+++ API/src/assets/ubeAPI.ts
@@ -11,8 +11,6 @@ const getPricesUber = async () => {
             "sec-fetch-site": "same-origin",
             "sec-gpc": "1",
             "x-csrf-token": "x",
-            "x-uber-minfo": "JIGGKONOSTUSDHTPLINMHKHM",
-            "x-uber-wa-info": "NMGGIMNOGHJHLPPLQNPONQHM",
             "cookie": process.env.COOKIE_UBER,
             "Referer": "https://m.uber.com/looking?drop%5B0%5D=%7B%22latitude%22%3A-3.8309824%2C%22longitude%22%3A-38.5221671%2C%22addressLine1%22%3A%22R.%20Jardel%20Lima%20dos%20Santos%2C%20230%22%2C%22addressLine2%22%3A%22Fortaleza%20-%20CE%22%2C%22id%22%3A%22ChIJk6063NVPxwcR4AQ2Vyd-hSI%22%2C%22provider%22%3A%22google_places%22%2C%22index%22%3A0%7D&pickup=%7B%22latitude%22%3A-3.7560474%2C%22longitude%22%3A-38.4893449%2C%22addressLine1%22%3A%22Iguatemi%20Bosque%22%2C%22addressLine2%22%3A%22Av.%20Washington%20Soares%2C%2085%2C%20Fortaleza%20-%20CE%22%2C%22id%22%3A%22ChIJ_2UYIGNJxwcRH4FygqPNdy0%22%2C%22provider%22%3A%22google_places%22%7D&vehicle=11803",
             "Referrer-Policy": "strict-origin-when-cross-origin"
